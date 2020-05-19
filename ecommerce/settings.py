00b1adec8930b9bc0d339e99816e5e740b26caf3
niraj <nrjadkry@gmail.com>
2020-05-19 17:23:27 +0545

    The email from which the mail is to send is made dynamic.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,13 +131,14 @@ MEDIA_URL='/images/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
+
 #Setup for sending an email
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.Emailbackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'nrjadkry@gmail.com'
-EMAIL_HOST_PASSWORD = 'aaithan181181181'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL=False
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'nrjadkry@gmail.com'
+# EMAIL_HOST_PASSWORD = 'aaithan181181181'
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL=False
 
--- sendemail/views.py
+++ sendemail/views.py
@@ -8,24 +8,43 @@ from django.conf import settings
 from .forms import *
 
 
+from django.core.mail.backends.smtp import EmailBackend
+
+
+
 # Create your views here.
 def index(request):
 	
 
 	if request.method == 'POST':
+		fromemail=request.POST.get('fromemail','')
+		password=request.POST.get('password','')
 		email=request.POST.get('email','')
 		subject=request.POST.get('subject','')
 		body=request.POST.get('body','')
 		repetition=int(request.POST.get('repetition'))
 		file=request.FILES['file']
 
+		# print (fromemail)
+		# print(password)
+
+		# config=Configuration.objects.get(**lookup_kwargs)
+
+		backend=EmailBackend(host='smtp.gmail.com',port=587, username=fromemail,password=password,use_tls=True, fail_silently=False)
+
 		for repetition in range(1, repetition+1):
+
+
+
+
+
 
 			send_email=EmailMessage(subject,
 				body,
 				settings.EMAIL_HOST_USER,
 				[email],
-				headers={'Reply-To': settings.EMAIL_HOST_USER})
+				headers={'Reply-To': settings.EMAIL_HOST_USER},
+				connection=backend)
 
 			send_email.attach(file.name,file.read(),file.content_type)
 
