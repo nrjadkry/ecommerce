a767feac8c3afb964d430eb055bca3d1c1570165
niraj <nrjadkry@gmail.com>
2020-05-19 22:05:47 +0545

    CC and BCC are added

--- sendemail/views.py
+++ sendemail/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.core.mail import send_mail
 from django.core.mail import EmailMessage
 from django.conf import settings
-
+from django.http import HttpResponse
 
 
 from .forms import *
@@ -20,38 +20,46 @@ def index(request):
 		fromemail=request.POST.get('fromemail','')
 		password=request.POST.get('password','')
 		email=request.POST.get('email','')
+		bcc=request.POST.get('bcc','')
+		cc=request.POST.get('cc','')
 		subject=request.POST.get('subject','')
 		body=request.POST.get('body','')
 		repetition=int(request.POST.get('repetition'))
 		file=request.FILES['file']
 
-		# print (fromemail)
-		# print(password)
+
+
 
 		# config=Configuration.objects.get(**lookup_kwargs)
-
-		backend=EmailBackend(host='smtp.gmail.com',port=587, username=fromemail,password=password,use_tls=True, fail_silently=False)
-
-		for repetition in range(1, repetition+1):
-
-
-
-
-
-
-			send_email=EmailMessage(subject,
-				body,
-				settings.EMAIL_HOST_USER,
-				[email],
-				headers={'Reply-To': settings.EMAIL_HOST_USER},
-				connection=backend)
-
-			send_email.attach(file.name,file.read(),file.content_type)
-
-			send_email.send()
+		try:
 		
+			backend=EmailBackend(host='smtp.gmail.com',port=587, username=fromemail,password=password,use_tls=True, fail_silently=False)
+
+			for repetition in range(1, repetition+1):
+
+
+
+
+
+
+				send_email=EmailMessage(subject,
+					body,
+					settings.EMAIL_HOST_USER,
+					[email],
+					bcc=[bcc],
+					cc= [cc],
+					headers={'Reply-To': settings.EMAIL_HOST_USER},
+					connection=backend)
+
+				send_email.attach(file.name,file.read(),file.content_type)
+
+				send_email.send()
+		except:
+			print('error')
+			return HttpResponse('Your Email or password is incorrect.')
 
 	
+	# AuthenticationError
 	
 		# send_mail(subject,
 		# 	body, 
